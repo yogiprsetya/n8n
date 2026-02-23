fb1ec45a06317d529e2d5fb5f2b37ff67e5f05c9
Yogi Prasetya <yogiprsetya@gmail.com>
2026-02-23 09:25:58 +0700

    first commit

---
+++ start-n8n.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export DB_TYPE=postgresdb
+export DB_POSTGRESDB_HOST=127.0.0.1
+export DB_POSTGRESDB_PORT=54332
+export DB_POSTGRESDB_DATABASE=postgres
+export DB_POSTGRESDB_USER=postgres
+export DB_POSTGRESDB_PASSWORD=postgres
+export DB_POSTGRESDB_SCHEMA=public
+
+export N8N_PORT=1118
+export GENERIC_TIMEZONE=Asia/Jakarta
+
+n8n start
